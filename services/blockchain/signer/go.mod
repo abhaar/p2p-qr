11d9b0bfb2c4a5913a9600af0d77c7a51d3b4196
abhaar <abhaar@gmail.com>
2026-06-13 17:06:26 -0400

    signer changes

--- services/blockchain/signer/go.mod
+++ services/blockchain/signer/go.mod
@@ -32,11 +32,15 @@ require (
 	github.com/tklauser/go-sysconf v0.3.12 // indirect
 	github.com/tklauser/numcpus v0.6.1 // indirect
 	go.opentelemetry.io/auto/sdk v1.2.1 // indirect
-	go.opentelemetry.io/otel v1.40.0 // indirect
-	go.opentelemetry.io/otel/metric v1.40.0 // indirect
-	go.opentelemetry.io/otel/trace v1.40.0 // indirect
+	go.opentelemetry.io/otel v1.43.0 // indirect
+	go.opentelemetry.io/otel/metric v1.43.0 // indirect
+	go.opentelemetry.io/otel/trace v1.43.0 // indirect
 	golang.org/x/crypto v0.48.0 // indirect
-	golang.org/x/sync v0.19.0 // indirect
+	golang.org/x/net v0.51.0 // indirect
+	golang.org/x/sync v0.20.0 // indirect
 	golang.org/x/sys v0.42.0 // indirect
+	golang.org/x/text v0.34.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20260226221140-a57be14db171 // indirect
+	google.golang.org/grpc v1.81.1 // indirect
 	google.golang.org/protobuf v1.36.11 // indirect
 )
--- services/blockchain/signer/internal/service/service.go
+++ services/blockchain/signer/internal/service/service.go
@@ -5,29 +5,23 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/p2p/blockchain/signer/v2/api"
 	"github.com/p2p/blockchain/signer/v2/internal/evm"
 	"github.com/p2p/shared/pb/blockchain/network"
+	"github.com/p2p/shared/pb/blockchain/signer"
 )
-
-var _ api.Signer = (*InMemorySigner)(nil)
 
 type InMemorySigner struct{}
 
-func (s *InMemorySigner) SignTransaction(ctx context.Context, req *api.SignTransactionRequest) (*api.SignTransactionResponse, error) {
+func (s *InMemorySigner) SignTransaction(ctx context.Context, req *signer.UnsignedTransactionRequest) (*signer.SignedTransaction, error) {
 	if req == nil {
 		return nil, fmt.Errorf("request cannot be nil")
 	}
 
-	if req.NetworkID != network.NetworkId_NETWORK_ID_ETHEREUM {
-		return nil, fmt.Errorf("unsupported network: %s", req.NetworkID.String())
-	}
-
-	switch req.Intent.Type {
-	case api.IntentTypeErc20Transfer:
-		return evm.SignErc20TransferIntent(req.Intent)
-
+	switch req.GetNetworkId() {
+	case network.NetworkId_NETWORK_ID_ETHEREUM:
+		evmUnsignedTransactionRequest := req.GetEvm()
+		return evm.SignTransferIntent(evmUnsignedTransactionRequest)
 	default:
-		return nil, fmt.Errorf("unsupported intent type on Ethereum: %s", req.Intent.Type)
+		return nil, fmt.Errorf("network not supported: %s", req.GetNetworkId())
 	}
 }
